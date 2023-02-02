faa2780b389f3e4a2719e54fcb92cb7a885d5140
Raphael Huc <raph82130@gmail.com>
2023-02-02 11:21:34 +0100

    Feat: Add Timer and resources

--- app/src/main/java/helloandroid/ut3/battlewhat/activity/GameActivity.java
+++ app/src/main/java/helloandroid/ut3/battlewhat/activity/GameActivity.java
@@ -3,15 +3,42 @@ package helloandroid.ut3.battlewhat.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.widget.Chronometer;
 
 import helloandroid.ut3.battlewhat.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    private Chronometer timer;
+    private boolean isRunning;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        isRunning = false;
+        timer = (Chronometer) findViewById(R.id.timer);
+        start();
+    }
+
+    /**
+     * Start the game
+     */
+    public void start() {
+        if(!isRunning || timer.isCountDown()) {
+            timer.start();
+            isRunning = true;
+        }
+    }
+
+    /**
+     * Stop the game
+     */
+    public void stop() {
+        if(isRunning || !timer.isCountDown()) {
+            timer.stop();
+            isRunning = false;
+        }
     }
 }
