3443e155a1835187e043d3d49c44f811360ff02a
jossyyila <jossyyila@gmail.com>
2023-03-12 00:28:48 +0100

    #5 update player mvt

--- app/src/main/java/helloandroid/ut3/battlewhat/activity/GameActivity.java
+++ app/src/main/java/helloandroid/ut3/battlewhat/activity/GameActivity.java
@@ -1,17 +1,15 @@
 package helloandroid.ut3.battlewhat.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Chronometer;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import helloandroid.ut3.battlewhat.R;
@@ -39,18 +37,14 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
     private int gameContent_height;
 
     private boolean mvtEnemy = true;
+    private boolean mvtPlayer = false;
 
-    /**
-     * un Runnable qui sera appelé par le timer pour la gestion du mouvement du player
-     */    private Runnable mUpdatePlayerPositionTime = new Runnable() {
-        public void run() {
-            mHandler.postDelayed(this, 20);
-            if(playerPosition < gameContent_height - 110){
-                playerPosition+=2;
-                player.setY(playerPosition);
-            }
-        }
-    };
+    private int vitesseEnemy = 4;
+    private int vitessePlayer = 0;
+
+    private float lastY;
+    private long startTimeSwipe;
+    private static final int SWIPE_THRESHOLD = 100;
 
     /**
      * un Runnable qui sera appelé par le timer pour la gestion du mouvement de l'enemy
@@ -59,13 +53,13 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
         public void run() {
             mHandler.postDelayed(this, 20);
             if (mvtEnemy){
-                enemyPosition+=4;
+                enemyPosition+=vitesseEnemy;
                 if(enemyPosition >= gameContent_height - 110){
                     mvtEnemy = !mvtEnemy;
                 }
             }
             if (!mvtEnemy) {
-                enemyPosition-=4;
+                enemyPosition-=vitesseEnemy;
                 if (enemyPosition <= 0) {
                     mvtEnemy = !mvtEnemy;
                 }
@@ -98,9 +92,9 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
             }
         });
 
-        mHandler = new Handler();
+        //mHandler = new Handler();
         //exécuter cette fonction au bout de time secondes
-        mHandler.postDelayed(mUpdatePlayerPositionTime, 1000);
+        //mHandler.postDelayed(mUpdatePlayerPositionTime, 1000);
 
         player = findViewById(R.id.player);
         playerPosition = gameContent_height;
@@ -163,19 +157,91 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
     }
 
     /**
-     * When we touche screen, player go up
+     * When we touche screen, move player
      */
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        if (playerPosition > 0){
-            playerPosition-=40;
-            /*ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) player.getLayoutParams();
-            params.topMargin = 100;
-            player.setLayoutParams(params);*/
-            player.setY(playerPosition);
-            //System.out.println("playerPosition: " + playerPosition + " Positions"+event.getX() + " : " + event.getY()+" Position "+player.getY());
+        switch(event.getAction()) {
+            case (MotionEvent.ACTION_DOWN) :
+                // Enregistre la position de départ du toucher
+                lastY = event.getY();
+                // Enregistre le temps de départ
+                startTimeSwipe = SystemClock.elapsedRealtime();
+                v.setTag(startTimeSwipe); // Stocke la valeur de startTime dans la propriété tag de la vue
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                // Calcule la distance parcourue depuis le toucher initial
+                float deltaY = event.getY() - lastY;
+                // Calcule la distance parcourue depuis le toucher initial
+                //float deltaY = event.getY() - v.getY();
+                if (deltaY > SWIPE_THRESHOLD) {
+                    // Le swipe est vers le bas
+                    // Ajoutez votre code ici pour traiter le swipe vers le bas
+                    mvtPlayer = false;
+                    return true; // Indique que l'événement a été traité
+                } else if (deltaY < -SWIPE_THRESHOLD) {
+                    // Le swipe est vers le haut
+                    // Ajoutez votre code ici pour traiter le swipe vers le haut
+                    mvtPlayer = true;
+                    return true;
+                }
+            case (MotionEvent.ACTION_UP) :
+                long startTime = (Long) v.getTag(); // Récupère la valeur de startTime à partir de la propriété tag de la vue
+                long elapsedTime = SystemClock.elapsedRealtime() - startTime; // Calcule le temps écoulé
+                startUpdatePlayerPositionTimeWithDuration(4, elapsedTime);
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                return true;
+            default :
+                return super.onTouchEvent(event);
         }
-        return false;
+    }
+
+    /**
+     * un Runnable qui sera appelé par le timer pour la gestion du mouvement du player
+     */
+    private Runnable mUpdatePlayerPositionTime = new Runnable() {
+        public void run() {
+            // mettre à jour la position du joueur
+            mHandler.postDelayed(this, 20);
+            updatePosition(vitessePlayer);
+        }
+    };
+
+    // méthode pour démarrer l'exécution du Runnable initial avec une durée spécifiée
+    private void startUpdatePlayerPositionTimeWithDuration(int speed, long duration) {
+        vitessePlayer = speed; // modifier la valeur de vitessePlayer
+        mHandler.post(mUpdatePlayerPositionTime);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                vitessePlayer = 0;
+                stopUpdatePlayerPositionTime();
+            }
+        }, duration);
+    }
+
+    // méthode pour arrêter spécifiquement l'exécution du Runnable initial
+    private void stopUpdatePlayerPositionTime() {
+        mHandler.removeCallbacks(mUpdatePlayerPositionTime);
+    }
+
+    public void updatePosition(int vitessePlayer) {
+        //go down
+        if (!mvtPlayer){
+            if(playerPosition < gameContent_height - 110){
+                playerPosition+=vitessePlayer;
+            }
+        }
+        //go up
+        if (mvtPlayer){
+            if(playerPosition >= 0){
+                playerPosition-=vitessePlayer;
+            }
+        }
+        player.setY(playerPosition);
     }
 
 }
--- app/src/main/java/helloandroid/ut3/battlewhat/activity/MainMenuActivity.java
+++ app/src/main/java/helloandroid/ut3/battlewhat/activity/MainMenuActivity.java
@@ -21,7 +21,7 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        sensorsBattleWhat = new SensorsBattleWhat(this);
+        //sensorsBattleWhat = new SensorsBattleWhat(this);
     }
 
     public void startConnectHighscore(View view) {
