25f0f216b50774bbed4d5a4b26a519a38375c8d0
matias248 <matias.andorra@gmail.com>
2023-03-16 19:28:05 +0100

    feat:v1 IA

--- app/src/main/java/helloandroid/ut3/battlewhat/object/spaceship/EnemySpaceShip.java
+++ app/src/main/java/helloandroid/ut3/battlewhat/object/spaceship/EnemySpaceShip.java
@@ -1,19 +1,20 @@
 package helloandroid.ut3.battlewhat.object.spaceship;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.view.View;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import helloandroid.ut3.battlewhat.R;
-import helloandroid.ut3.battlewhat.activity.GameActivity;
-import helloandroid.ut3.battlewhat.object.Shot;
 
 public class EnemySpaceShip extends SpaceShip {
+
+    private boolean youNeedToAvoid=false;
+    private boolean dodgeNextMove=false;
+    private int noNewActionUntil =0;
+
 
     public EnemySpaceShip(Context context, View spaceShipView) {
         super(context, spaceShipView);
@@ -26,5 +27,35 @@ public class EnemySpaceShip extends SpaceShip {
                 (int) getPositionY(),
                 (int) (getPositionX() + getWidth()),
                 (int) (getPositionY() + getHeight()));
+    }
+    public Rect getPredictCollisionShape () {
+        return new Rect(
+                (int) getPositionX()+getWidth(),
+                (int) getPositionY(),
+                (int) (getPositionX() + getWidth()*2),
+                (int) (getPositionY() + getHeight()));
+    }
+    public boolean getYouNeedToAvoid() {
+        return youNeedToAvoid;
+    }
+
+    public boolean getDodgeNextMove() {
+        Random rand = new Random();
+        int dodgeNextMoveProba= rand.nextInt(100)+1;
+        return dodgeNextMoveProba>90;
+    }
+    public void setYouNeedToAvoid(Boolean youNeedToAvoid) {
+        this.youNeedToAvoid = youNeedToAvoid;
+    }
+
+    public int getNoNewActionUntil() {
+        return noNewActionUntil;
+    }
+
+    public void setNoNewActionUntil(int noNewActionUntil) {
+        this.noNewActionUntil = noNewActionUntil<0 ? noNewActionUntil : noNewActionUntil;
+    }
+    public void reduceNoNewActionUntil(int reducer){
+        this.noNewActionUntil -= noNewActionUntil<reducer ?0 : reducer;
     }
 }
